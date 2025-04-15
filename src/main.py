49da145c2cba1c3d7ccaacc79b2262b81194e086
Luis Gabriel Goés de Santana <luisgabriel533@gmail.com>
2025-04-15 14:28:14 -0300

    feat: Adição do prompt handling.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 
+# Define o template de prompt com placeholders que serão preenchidos dinamicamente
+template = """
+Answer the question below.
+
+Here is the conversation history:
+{context}
+
+Question:
+{question}
+
+Answer:
+"""
+
+# Criação do modelo e template do prompt
 model = OllamaLLM(model="llama3")
+prompt = ChatPromptTemplate.from_template(template)
 
-ollamaResponse = model.invoke(input="Hello World!")
-print(ollamaResponse)
+# Criação da cadeia (prompt -> modelo)
+chain = prompt | model
+
+# Executa a cadeia com os valores fornecidosf
+response = chain.invoke({
+    "context": "",
+    "question": "Hey, how are you?"
+})
+
+# Exibe a resposta
+print(response)
